98cff35d7fead5743aa06dc022fc2f42b9322bd0
Mike Grue <MikeDavidG2@yahoo.com>
2017-05-17 12:10:28 -0700

    improved List_Fields.py

--- List_Fields.py
+++ List_Fields.py
@@ -16,9 +16,10 @@ import arcpy
 
 # Change the below variable to print the fields at this table
 # It should be a full path and FC or Table name
-FC_or_table = r'Database Connections\AD@ATLANTIC@SDE.sde\SDE.SANGIS.ECO_VEGETATION_CN'
+FC_or_table = r'E:\SAN DIEGO\GIS\Historic_Addresses.shp'
 
-
+# Decide if you want the list returned as it is ordered in ArcCatalog or in alphabetical order
+alpha_or_as_ordered = 'alpha'
 
 #-------------------------------------------------------------------------------
 def main():
@@ -27,8 +28,21 @@ def main():
 
     print 'Getting the fields in:\n  %s:\n' % (FC_or_table)
 
-    for field in fields:
-        print field.name
+    # Print as ordered in ArcCatalog
+    if alpha_or_as_ordered == 'as_ordered':
+        for field in fields:
+            print field.name
+
+    # Print in alphabetical order
+    elif alpha_or_as_ordered == 'alpha':
+        f_list = []
+        for field in fields:
+            f_list.append(field.name)
+
+        f_list.sort()
+
+        for field in f_list:
+            print field
 
     print '\n'
 
