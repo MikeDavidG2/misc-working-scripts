596cb3e26b782a08e6b2e6e133543b2147ef6909
Mike Grue <MikeDavidG2@yahoo.com>
2017-09-21 16:01:12 -0700

    first commit of map_drives.bat

---
+++ map_drives.bat
@@ -0,0 +1,12 @@
+::This script maps the drives on the BLUE Network
+::Author, Gary Ross
+net use i: \\southern\imagery /P:yes
+net use p: \\southern\projects /P:yes
+net use s: \\southern\source_data /P:yes
+net use u: \\southern\users /P:yes
+net use v: \\southern\sde /P:yes
+net use x: \\southern\scratch /P:yes
+
+net use l: \\southern\lis /P:yes
+net use m: \\southern\sde_maintenance /P:yes
+net use z: \\southern\software /P:yes
